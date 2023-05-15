0b4b9ddb2d1973b959668d1b05ba2bc43025b099
EstebanNeorenFranco <estebanneorenfranco@gmail.com>
2023-05-15 11:43:06 -0300

    fixed hover awards

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector('.header_icons');
 const closeBtn = document.querySelector('.close_menu');
+let darkMode = false;
+const darkBtn = document.querySelector('.moon');
 
 if (menuBtn) {
   menuBtn.addEventListener('click', function() {
@@ -17,4 +19,15 @@ if (closeBtn) {
   });
 } else {
   console.error("No se encontró el botón de cerrar");
+}
+
+if (darkBtn) {
+  if (darkMode === true) {
+    darkBtn.addEventListener('click', function() {
+      document.getElementById('body').classList.remove('active');
+      document.getElementById('body').classList.add('closed');
+    });
+  }
+} else {
+  console.error("No se pudo activar el modo oscuro");
 }
