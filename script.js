c4b68fa4891c8def8ba320eba82d139c32b5a328
EstebanNeorenFranco <estebanneorenfranco@gmail.com>
2023-05-15 20:01:46 -0300

    darkmode device theme

--- script.js
+++ script.js
@@ -3,26 +3,36 @@ const closeBtn = document.querySelector('.close_menu');
 let darkMode = localStorage.getItem('darkMode') === 'true';
 const darkBtn = document.querySelector('.moon');
 
-// Función para habilitar el modo oscuro
+
 function enableDarkMode() {
   darkMode = true;
   document.body.classList.add('dark-mode');
   localStorage.setItem('darkMode', 'true');
 }
 
-// Función para deshabilitar el modo oscuro
+
 function disableDarkMode() {
   darkMode = false;
   document.body.classList.remove('dark-mode');
   localStorage.setItem('darkMode', 'false');
 }
 
-// Función para alternar el modo oscuro
+
 function toggleDarkMode() {
   if (darkMode) {
     disableDarkMode();
   } else {
     enableDarkMode();
+  }
+}
+
+function checkDeviceTheme() {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  
+  if (prefersDarkMode) {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
   }
 }
 
@@ -53,4 +63,14 @@ if (darkBtn) {
 
 if (darkMode) {
   enableDarkMode();
+}
+
+function checkDeviceTheme() {
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  
+  if (prefersDarkMode) {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
 }
