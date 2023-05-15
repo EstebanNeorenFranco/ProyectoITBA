3c40872ffbbefbb77906833d2ce20f4c1f861196
EstebanNeorenFranco <estebanneorenfranco@gmail.com>
2023-05-15 19:51:53 -0300

    darkmode localStorage

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const menuBtn = document.querySelector('.header_icons');
 const closeBtn = document.querySelector('.close_menu');
-let darkMode = false;
+let darkMode = localStorage.getItem('darkMode') === 'true';
 const darkBtn = document.querySelector('.moon');
+
+// Función para habilitar el modo oscuro
+function enableDarkMode() {
+  darkMode = true;
+  document.body.classList.add('dark-mode');
+  localStorage.setItem('darkMode', 'true');
+}
+
+// Función para deshabilitar el modo oscuro
+function disableDarkMode() {
+  darkMode = false;
+  document.body.classList.remove('dark-mode');
+  localStorage.setItem('darkMode', 'false');
+}
+
+// Función para alternar el modo oscuro
+function toggleDarkMode() {
+  if (darkMode) {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
+}
 
 if (menuBtn) {
   menuBtn.addEventListener('click', function() {
@@ -22,15 +45,12 @@ if (closeBtn) {
 }
 
 if (darkBtn) {
-  darkBtn.addEventListener('click', function() {
-    darkMode = !darkMode;
-
-    if (darkMode) {
-      document.body.classList.add('dark-mode');
-    } else {
-      document.body.classList.remove('dark-mode');
-    }
-  });
+  darkBtn.addEventListener('click', toggleDarkMode);
 } else {
   console.error("No se encontró el botón de modo oscuro");
+}
+
+
+if (darkMode) {
+  enableDarkMode();
 }
